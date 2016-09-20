4d8f8cbebfd30282115ad4827163f86d807e9194
Paul Irwin <paulirwin@gmail.com>
2016-09-20 18:08:22 -0400

    Initial commit

--- UNFHelloWorld/Controllers/HomeController.cs
+++ UNFHelloWorld/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UNFHelloWorld.Models;
 
 namespace UNFHelloWorld.Controllers
 {
     public class HomeController : Controller
     {
+        HelloDbContext _context = new HelloDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var data = _context.HelloData.ToList();
+
+            return View(data);
         }
 
         public ActionResult About()
---
+++ UNFHelloWorld/Models/HelloDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+
+namespace UNFHelloWorld.Models
+{
+    public class HelloDbContext : DbContext
+    {
+        public HelloDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<HelloData> HelloData { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HelloData>().ToTable("HelloData");
+        }
+    }
+
+    public class HelloData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
